c63f6fb89faddfb13f56b2bf2f3ab15fda4a5f07
Tech <nkwawirbeltus2222@gmail.com>
2023-11-08 15:35:43 +0300

    added app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import gradio as gr
+
+def greet(name):
+
+    if isinstance(name, str):
+
+        return "Hello " + name + "!"
+    
+    else:
+        return "String is expected"
+
+
+def isEven(n1, n2):
+
+    s = n1 + n2
+
+    if s%2==0:
+        results = 'Sum is Even'
+
+    else:
+        results = 'Sum is Odd'
+
+    return results
+
+demo = gr.Interface(fn=isEven, inputs=["number" , "number"], outputs="text")
+    
+demo.launch()   
